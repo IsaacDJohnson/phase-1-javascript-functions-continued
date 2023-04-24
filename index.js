8d09a6ac26f19344eef3f3ff9f1c1ab4ce6e5086
IsaacDJohnson <isaac.designws@gmail.com>
2023-04-24 12:54:30 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+saturdayFun("bathe my dog")
+
+function mondayWork(work = "go to the office") {
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(string = "*"){
+    const innerFunction = function (word = special) {
+        return `You are ${string}${word}${string}!` 
+    }
+    return innerFunction
+}
+wrapAdjective("%")("a dedicated programmer")
